6143d55daabe77f39556586b8427c0523d678b64
Piotr Bugara <piotr.bugara@gmail.com>
2025-04-15 08:36:28 +0200

    Project initialization

---
+++ src/test/kotlin/com/zen/recruitment/RecruitmentApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.zen.recruitment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest
+class RecruitmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
